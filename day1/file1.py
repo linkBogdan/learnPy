23e7042499e3a34a691ff079f8592a613756040f
Bogdan <4bogdanalexandru@gmail.com>
2025-10-22 12:27:04 +0300

    Simple program to check if a number is even or odd.

---
+++ day1/file1.py
@@ -0,0 +1,15 @@
+# learnPy/day1/file1.py
+# This program asks the user for a number and tells if it is even or odd.
+
+# Ask the user to enter a number.
+number = input("Enter a number: ")
+
+# Convert the input to an integer.
+# (This will raise an error if the input is not a valid integer.)
+number = int(number)
+
+# Use modulo operator to check for evenness.
+if number % 2 == 0: 
+    print(f"{number} is even.") # Number is divisible by 2
+else:
+    print(f"{number} is odd.") # Number is not divisible by 2
