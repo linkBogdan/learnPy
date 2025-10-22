fcdeb88eb74b28333867e5eb3249bb7ba3f6173c
Bogdan <4bogdanalexandru@gmail.com>
2025-10-22 20:39:26 +0300

    Function now handles repetition and mentains order of input

--- day1/file3.py
+++ day1/file3.py
@@ -1,17 +1,26 @@
 # learnPy/day1/file3.py
+# Small program to collect numbers from the user into a list
 
 from file1 import get_number  # Importing the get_number function from file1.py
 
-numbers = []  # List to store user-entered numbers
-
-while True:
-    number = get_number(prompt="Enter a number to add to the list (or type 'stop' to finish): ", stop_on_empty=True)
-    if number is None:
-        break
-    numbers.append(number)
-
-# Display the collected numbers after exiting the loop
-if not numbers:
-    print("No numbers were entered.")
-else:
-    print("You entered the following numbers:", numbers)
+prompt = "Enter a number to add to the list (or type 'stop' to finish):"
+def collect_numbers(prompt, stop_on_empty=True, allow_repetition=True):
+    """
+    Collect numbers from the user until they choose to stop.
+    Returns a list of collected integers.
+    """
+    numbers = []
+    unique_numbers = []
+    while True:
+        number = get_number(prompt=prompt, stop_on_empty=stop_on_empty)
+        if number is None: # This handles the empty input
+            break
+        numbers.append(number) # Could be optimized based on allow_repetition but kept simple here
+        if allow_repetition is False:
+            if number not in unique_numbers:
+                unique_numbers.append(number)
+    return unique_numbers if not allow_repetition else numbers
+    
+# Main program
+my_numbers = collect_numbers(prompt, stop_on_empty=True, allow_repetition=False)
+print("You entered:", my_numbers)
