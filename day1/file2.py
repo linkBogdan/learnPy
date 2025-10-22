f20579eeae46c30057721e10acf4bc30bc3e5d25
Bogdan <4bogdanalexandru@gmail.com>
2025-10-22 16:55:07 +0300

    It now prompts until you guess, stops on stop or empty input

--- day1/file2.py
+++ day1/file2.py
@@ -6,10 +6,17 @@ print("Welcome to the Number Guessing Game!")
 print("I have selected a random number between 1 and 20.")
 print("Can you guess what it is?")
 random_number = random.randint(1, 20)
-guess = get_number(prompt= "Take a guess: ")
 
-if guess == random_number:
-    print("Congratulations! You guessed the correct number.")
-else:
-    print(f"Sorry, the correct number was {random_number}.")    
-
+while True:
+    guess = get_number(prompt= "Take a guess: ", stop_on_empty=True)
+    if guess is None:
+        print("Gave up already? The number was", random_number, ".")
+        break
+    elif guess < random_number:
+        print("You guessed too low.")
+    elif guess > random_number:
+        print("You guessed too high.")
+    elif guess == random_number:
+        print("Bingo! It was ", random_number, "!")
+        break
+    
