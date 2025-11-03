206ef8bfffb1d9b518ff8d494f5e7ddb82daad61
Bogdan <4bogdanalexandru@gmail.com>
2025-11-03 18:06:44 +0200

    Refactored to use better approach and create directory

--- day10/file7.py
+++ day10/file7.py
@@ -1,14 +1,32 @@
 # Refactor for this:
 
 from datetime import date
+import os
 
+def get_path(): 
+    """ Retrun the path to the root. """
+    root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
+    return root_path
+
+def create_today_dir():
+    dir_name = get_day()
+
+    root = get_path()
+    full_path = os.path.join(root, dir_name)
+
+    if not os.path.exists(full_path):
+        os.makedirs(full_path)
+    else:
+        print(f"{dir_name} already exists")    
 
 def get_day():
-    # We define when we started working
+    """ Return today's day folder name and start date"""
+
     start_date = date(2025, 10, 22)
     today = date.today()
 
     days_passed = (today - start_date).days + 1 # +1 if day 1 = start day
+    dir_format_name = f"day{days_passed}"
     
-    return days_passed
+    return dir_format_name
 
