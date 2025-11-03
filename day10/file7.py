1b4675d697ff9c2003dfdecda65920d182a791d4
Bogdan <4bogdanalexandru@gmail.com>
2025-11-03 14:25:59 +0200

    get_match function

--- day10/file7.py
+++ day10/file7.py
@@ -5,11 +5,14 @@ import re
     
 # Function that lists every directory that starts with 'day'.
 def list_dir():
-    
+    directories = []
+
     with os.scandir(path=get_path()) as it:
         for entry in it:
             if entry.name.startswith('day') and entry.is_dir():
-                print(entry.name)
+                directories.append(entry)
+    return directories
+    
 
 
 # This will print the path to the root    
@@ -17,10 +20,9 @@ def get_path():
     root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     return root_path
 
-entries = list(list_dir())
-for entry in entries:
-    match = re.match(r"day(\d+)$", list_dir())
+def get_match():
+    entries = list_dir()
 
-
-x = get_path()
-print (x)
+    for entry in entries:
+        match = re.match(r"day(\d+)$", entry.name)
+        print(match)
